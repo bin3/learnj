36a92aee0136f35258cb2345ced40df1b6e7b537
zhangwenbin <zhangwenbin@benbun.com>
2016-09-24 15:20:44 +0800

    refactor: JdbcDemo

--- src/main/java/learnj/db/JdbcDemo.java
+++ src/main/java/learnj/db/JdbcDemo.java
@@ -18,50 +18,74 @@ import java.sql.Statement;
  */
 public class JdbcDemo {
 
-  public static void main(String[] args) throws Exception {
+  public static Connection connect() {
     String host = "devdb.benbun.com";
-//    String host = "127.0.0.1";
     String port = "3306";
     String username = "root";
     String password = "7_CiRzkJmBicZpje";
-//    String password = "";
     String database = "test";
 
-    Connection conn = null;
-    String sql;
-    String url = String.format(
+    String url =
+        String
+            .format(
                 "jdbc:mysql://%s:%s/%s?user=%s&password=%s&useUnicode=true&characterEncoding=UTF8&serverTimezone=UTC&useSSL=false",
                 host, port, database, username, password);
 
+    Connection conn;
     try {
       conn = DriverManager.getConnection(url);
-      Statement stmt = conn.createStatement();
-      int result;
-//      sql = "create table student(NO char(20),name varchar(20),primary key(NO))";
-//      result = stmt.executeUpdate(sql);// executeUpdate语句会返回一个受影响的行数，如果返回-1就没有成功
-//      if (result != -1) {
-//        System.out.println("创建数据表成功");
-//      }
+      return conn;
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
 
-      sql = "insert into student(NO,name) values('2012001','陶伟基')";
-      result = stmt.executeUpdate(sql);
-      sql = "insert into student(NO,name) values('2012002','周小俊')";
-      result = stmt.executeUpdate(sql);
-      sql = "select * from student";
+  public static boolean create(Statement stmt) {
+    String sql = "create table student(no char(20),name varchar(20),primary key(no))";
+    try {
+      int result = stmt.executeUpdate(sql);
+      return result != -1;
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  public static boolean insert(Statement stmt) {
+    String sql = "insert into student(no,name) values('2012001','陶伟基')";
+    try {
+      int result = stmt.executeUpdate(sql);
+      return result != -1;
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  public static boolean select(Statement stmt) {
+    String sql = "select * from student";
+    try {
       ResultSet rs = stmt.executeQuery(sql);
       System.out.println("学号\t姓名");
       while (rs.next()) {
         System.out.println(rs.getString(1) + "\t" + rs.getString(2));
       }
+      return true;
     } catch (SQLException e) {
-      System.out.println("MySQL操作错误");
       e.printStackTrace();
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      conn.close();
     }
+    return false;
+  }
 
+  public static void main(String[] args) throws Exception {
+
+    Connection conn = connect();
+    Statement stmt = conn.createStatement();
+    create(stmt);
+    insert(stmt);
+    select(stmt);
+    conn.close();
   }
 
 }
